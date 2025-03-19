22c6abb5245202685ea3711180a4e33b64de41bc
Macob2 <jacoborellana@liceovvh.cl>
2025-03-19 15:59:51 -0300

    ok

--- JS/07_ejercicios_if-else/script5.js
+++ JS/07_ejercicios_if-else/script5.js
@@ -12,3 +12,37 @@ function Chequeo_clima(){
         }
 }
 
+function Ver_usuario(){
+    let nombreUsuario = prompt("ingrese su usuario: ")
+    let contraseña = prompt("ingrese su contraseña: ")
+
+    if (nombreUsuario === "usuario1234" && contraseña === "secreto"){
+        alert("Su usuario es: " + nombreUsuario + "\nSu contraseña es: " + contraseña + "\nAcceso concedido") 
+    } else {
+        alert("Su usuario es: " + nombreUsuario + "\nSu contraseña es: " + contraseña + "\nAcceso denegado") 
+    }
+}
+
+function Valor_signo(){
+    let numero = prompt("ingresar un numero: ")
+
+    if (numero < 0) {
+        alert("el numero es: " + numero + " es negativo")
+    } else if (numero == 0){
+        alert("el numero es: " + numero + " el numero es cero")
+    } else {
+        alert("el numero es: " + numero + " el numero es positivo")
+    }
+}
+
+function Puntaje_juego(){
+    let puntuacion = prompt("ingrese su puntuacion: ")
+
+    if (puntuacion >= 90){
+        alert("Su puntuacion es: " + puntuacion + ", Excelente!")
+    } else if (puntuacion > 70){
+        alert("Su puntuacion es: " + puntuacion + ", Buen trabajo")
+    } else {
+        alert("Su puntuacion es: " + puntuacion + ", Necesitas mejorar")
+    }
+}
