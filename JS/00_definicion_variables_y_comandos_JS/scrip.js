6bf0741472c259e433deeed84b3cd116d705c97f
Macob2 <jacoborellana@liceovvh.cl>
2025-03-19 08:51:44 -0300

    ok

---
+++ JS/00_definicion_variables_y_comandos_JS/scrip.js
@@ -0,0 +1,9 @@
+console.log("Vinculado correctamente...")
+function Suma_indices(){
+
+    let indice = [1,2,3,4,5,6,7]
+    let suma = indice[0] + indice[1] + indice[2] + indice[3] + indice[4] + indice[5] + indice[6];
+
+    alert(`EL arreglo es ${indice.join(" - ")}:\nResultado: ${suma}`);
+    
+}
