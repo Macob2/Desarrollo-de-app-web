79450af8dbce342533b1a4d2c27ab98023a4aca2
Macob2 <jacoborellana@liceovvh.cl>
2025-07-08 11:19:54 -0400

    ok

--- JS/22_metodos_push_pop_unshift_shift/script.js
+++ JS/22_metodos_push_pop_unshift_shift/script.js
@@ -1,3 +1,30 @@
 
 let compras = []
-compras.push("pan", "leche" y "huevos")
+let colores = ["rojo", "azul"]
+let resultado = document.getElementById("resultado"); //referencia UL
+function supermercado(){
+    compras.push("pan","leche" ,"huevos")
+
+    alert(`Se a añadido al carrito: ${compras.join("-")}`)
+}
+
+function eliminarUltimo(){
+    compras.push("pan","leche" ,"huevos")
+
+    let compras_nuevas = compras.pop()
+    alert(`Los items que fueron eliminados fueron: ${compras_nuevas}`)
+}
+
+function agregarAlInicio(){
+    colores.unshift("Amarillo")
+
+    alert(`Los colores son: ${colores.join("-")}`)
+}
+
+function hola(){
+    let elemento = document.getElementById("lista").value; //valor input
+    let hola1 = document.createElement("li"); //crear elemento HTML --createElement
+    hola1.innerText = elemento; //<li>--innerText--</li>
+    resultado.appendChild(hola1) //appendChild inserta un valor hijo en el padre
+    //resultado(<ul>(item(<li></li>))</ul>)
+}
