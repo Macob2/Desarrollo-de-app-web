4d06d265038218a91049e16ea4fa5013599ca0e6
Macob2 <jacoborellana@liceovvh.cl>
2025-07-09 09:34:11 -0400

    ok

--- JS/22_metodos_push_pop_unshift_shift/script.js
+++ JS/22_metodos_push_pop_unshift_shift/script.js
@@ -1,25 +1,5 @@
 
-let compras = []
-let colores = ["rojo", "azul"]
-let resultado = document.getElementById("resultado"); //referencia UL
-function supermercado(){
-    compras.push("pan","leche" ,"huevos")
-
-    alert(`Se a añadido al carrito: ${compras.join("-")}`)
-}
-
-function eliminarUltimo(){
-    compras.push("pan","leche" ,"huevos")
-
-    let compras_nuevas = compras.pop()
-    alert(`Los items que fueron eliminados fueron: ${compras_nuevas}`)
-}
-
-function agregarAlInicio(){
-    colores.unshift("Amarillo")
-
-    alert(`Los colores son: ${colores.join("-")}`)
-}
+//let resultado = document.getElementById("resultado"); //referencia UL
 
 function hola(){
     let elemento = document.getElementById("lista").value; //valor input
@@ -27,4 +7,37 @@ function hola(){
     hola1.innerText = elemento; //<li>--innerText--</li>
     resultado.appendChild(hola1) //appendChild inserta un valor hijo en el padre
     //resultado(<ul>(item(<li></li>))</ul>)
+}
+
+let carrito = [];
+function mostrarListaCompra(){
+    const lista = document.getElementById("resultado2");
+    lista.innerHTML = ""; //Limpiar antes de mostrar
+
+    for (let i = 0; i < carrito.length; i++){
+        const li = document.createElement("li");
+        li.className ="list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `- ${carrito[i]}`;
+        lista.appendChild(li)
+    }
+}
+
+function agregarElemento2() {
+    const input = document.getElementById("lista2");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+    
+    if (valor === "") return;
+    carrito.push(valor); //Agregar valor al final
+    
+    input.value = "";
+    input.focus(); // agregamos foco hacia el input
+
+    mostrarListaCompra()
+}
+
+function eliminarUltimoElemento(){
+    if (carrito.length > 0){
+        carrito.pop();
+    }
+    mostrarListaCompra()
 }
