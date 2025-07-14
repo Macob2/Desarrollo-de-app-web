e8314c932952510cf121a181fa26f1a19d94c4d5
Macob2 <jacoborellana@liceovvh.cl>
2025-07-14 11:14:45 -0400

    ok

--- JS/22_metodos_push_pop_unshift_shift/script.js
+++ JS/22_metodos_push_pop_unshift_shift/script.js
@@ -41,3 +41,69 @@ function eliminarUltimoElemento(){
     }
     mostrarListaCompra()
 }
+
+let colores = []
+
+function mostrar_lista_colores(){
+    const lista = document.getElementById("resultado3");
+    lista.innerHTML =""; //Limpia antes de mostrar 
+
+    for (let i = 0; i < colores.length; i++){
+        const li = document.createElement("li");
+        li.className ="list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `- ${colores[i]}`;
+        lista.appendChild(li)
+    }
+}
+
+function agregar_inicio() {
+    const input = document.getElementById("lista3");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+    
+    if (valor === ""){
+        return;
+    };
+     
+    colores.unshift(valor) //Agregar valor al inicio
+    input.value = "";
+    input.focus(); // agregamos foco hacia el input
+
+    mostrar_lista_colores()
+}
+
+let colores2 = []
+
+function mostrar_lista_colores2(){
+    const lista = document.getElementById("resultado4");
+    lista.innerHTML =""; //Limpia antes de mostrar 
+
+    for (let i = 0; i < colores2.length; i++){
+        const li = document.createElement("li");
+        li.className ="list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `- ${colores2[i]}`;
+        lista.appendChild(li)
+    }
+}
+
+function agregar_inicio2() {
+    const input = document.getElementById("lista4");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+    
+    if (valor === ""){
+        return;
+    };
+     
+    colores2.unshift(valor) //Agregar valor al inicio
+    input.value = "";
+    input.focus(); // agregamos foco hacia el input
+
+    mostrar_lista_colores2()
+}
+
+function eliminar_primer_elemento(){
+    if (colores2.length > 0){
+        colores2.shift();
+    };
+
+    mostrar_lista_colores2()
+}
