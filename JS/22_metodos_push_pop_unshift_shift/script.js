3b6664a2e1b728e1ca4cd9f22d95d3464ce43baf
Macob2 <jacoborellana@liceovvh.cl>
2025-07-17 11:17:11 -0400

    ok

--- JS/22_metodos_push_pop_unshift_shift/script.js
+++ JS/22_metodos_push_pop_unshift_shift/script.js
@@ -107,3 +107,137 @@ function eliminar_primer_elemento(){
 
     mostrar_lista_colores2()
 }
+
+let numeros = [10, 20 ,30]
+
+function mostrar_lista_numeros(){
+    const lista = document.getElementById("resultado5");
+    lista.innerHTML =""; //Limpia antes de mostrar 
+
+    for (let i = 0; i < numeros.length; i++){
+        const li = document.createElement("li");
+        li.className ="list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `- ${numeros[i]}`;
+        lista.appendChild(li)
+    }
+}
+
+function agregar_inicio3() {
+    const input = document.getElementById("lista5");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+    
+    if (valor === ""){
+        return;
+    };
+     
+    numeros.unshift(valor) //Agregar valor al inicio
+    input.value = "";
+    input.focus(); // agregamos foco hacia el input
+
+    mostrar_lista_numeros()
+}
+
+function agregar_final(){
+    const input = document.getElementById("lista5");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+    
+    if (valor === "") return;
+    numeros.push(valor); //Agregar valor al final
+    
+    input.value = "";
+    input.focus(); // agregamos foco hacia el input
+
+    mostrar_lista_numeros()
+}
+
+function eliminar_primer_elemento2(){
+    if (numeros.length > 0){
+        numeros.shift();
+    }
+
+    mostrar_lista_numeros()
+}
+
+function eliminar_ultimo_elemento(){
+    if (numeros.length > 0){
+        numeros.pop();
+    }
+    mostrar_lista_numeros()
+}
+
+let orden = []
+
+function mostrar_lista_orden(){
+    const lista = document.getElementById("resultado6");
+    lista.innerHTML =""; //Limpia antes de mostrar 
+
+    for (let i = 0; i < orden.length; i++){
+        const li = document.createElement("li");
+        li.className ="list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `- ${orden[i]}`;
+        lista.appendChild(li)
+    }
+}
+
+function agregar_inicio_ej6(){
+    const input = document.getElementById("lista6");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+    
+    if (valor === ""){
+        return;
+    };
+     
+    orden.unshift(valor) //Agregar valor al inicio
+    input.value = "";
+    input.focus(); // agregamos foco hacia el input
+
+    mostrar_lista_orden()
+}
+
+let historial = []
+
+let mensjes_eliminados = []
+
+function mostrar_historial(){
+    const lista = document.getElementById("resultado7");
+    lista.innerHTML =""; //Limpia antes de mostrar 
+
+    for (let i = 0; i < historial.length; i++){
+        const li = document.createElement("li");
+        li.className ="list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `- ${historial[i]}`;
+        lista.appendChild(li)
+    }
+}
+
+function mostrar_lista_mensajes_eliminados(){
+    const lista = document.getElementById("resultado7.1");
+    lista.innerHTML =""; //Limpia antes de mostrar 
+
+    for (let i = 0; i < historial.length; i++){
+        const li = document.createElement("li");
+        li.className ="list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `- ${mensjes_eliminados[i]}`;
+        lista.appendChild(li)
+    }
+}
+
+function agregar_final_ej7(){
+    const input = document.getElementById("lista7");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+    
+    if (valor === "") return;
+    historial.push(valor); //Agregar valor al final
+    
+    input.value = "";
+    input.focus(); // agregamos foco hacia el input
+
+    mostrar_historial()
+}
+
+function eliminar_ultimo_mensaje_ej7(){
+    if (historial.length > 0){
+        historial.pop();
+    }
+    mostrar_historial()
+}
