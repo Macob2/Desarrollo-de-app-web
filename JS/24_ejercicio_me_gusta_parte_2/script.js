5642a3581544955eac48b7e1a6797a686be2e23a
Macob2 <165290780+Macob2@users.noreply.github.com>
2025-07-30 09:54:08 -0400

    ok

--- JS/24_ejercicio_me_gusta_parte_2/script.js
+++ JS/24_ejercicio_me_gusta_parte_2/script.js
@@ -1,6 +1,17 @@
 let Contador = 9;
-
+let Contador2 = 12;
+let Contador3 = 9;
 function Sumar_Likes(){
     Contador++;
     document.getElementById('Contador').textContent = Contador + " Megusta(s)";
+}
+
+function Sumar_Likes2(){
+    Contador2++;
+    document.getElementById('Contador-2').textContent = Contador2 + " Megusta(s)";
+}
+
+function Sumar_Likes3(){
+    Contador3++;
+    document.getElementById('Contador-3').textContent = Contador3 + " Megusta(s)";
 }
