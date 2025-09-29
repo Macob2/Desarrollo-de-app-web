416ab19578033232d05e7b2db31fe32d91174caf
Macob2 <jacoborellana@liceovvh.cl>
2025-09-29 10:19:31 -0300

    ok

--- JS/26_this_js/emeplo_this/static/js/script.js
+++ JS/26_this_js/emeplo_this/static/js/script.js
@@ -4,7 +4,7 @@ function example(elemento){
 //Creacion de funcion TurnOff
 function TurnOff(element){
     if (element.innerText == "Login"){
-        element.innerText = "Logout";
+        element.innerText = "Log out";
     }else{
         element.innerText = "Login"
     }
@@ -15,4 +15,15 @@ function hide(element) {
     element.remove();
 }
 
-function 
+let like_1 = 13
+let like_2 = 37
+
+function Like(element){
+    like_1++;
+    document.getElementById('like_container1').textContent = like_1 + " Like(s)";
+}
+
+function Like_2(element){
+    like_2++;
+    document.getElementById('like_container2').textContent = like_2 + " Like(s)";
+}
